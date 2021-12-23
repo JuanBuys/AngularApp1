f5e63ee347ddadd48fd1ba54a03027f7b88bc75c
JuanBuys <jbuys_54@yahoo.com>
2021-12-23 15:56:54 +0200

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -27,7 +27,7 @@ export class ProductDetailsComponent implements OnInit {
     this.product = products.find(product => product.id === productIdFromRoute);
   }
 
-  addToCart(product: Product){
+  addToCart(product: Product) {
     this.cartService.addToCart(product);
     window.alert('Your product has been added to the cart!');
   }
