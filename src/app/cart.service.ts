fb1e07291e76555a904616d6cb0dcd5c06a2ec42
JuanBuys <jbuys_54@yahoo.com>
2021-12-24 12:35:06 +0200

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -2,9 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
+@Injectable({
+  providedIn: 'root'
+})
+
 export class CartService {
   items: Product[] = [];
-  shippingCosts: []=[];
+  shippingCosts: []= [];
 
   constructor(
     private http: HttpClient
